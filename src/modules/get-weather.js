f1c6f0217be9fc049903a3c57c75708d4c76c27a
Sterka1402 <kozlova.valya@gmail.com>
2020-12-15 16:22:30 +0300

    add webpack ans eslint, get weather from web

---
+++ src/modules/get-weather.js
@@ -0,0 +1,24 @@
+const getHttp = (url) => new Promise((resolve, reject) => {
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', url, true);
+  xhr.send();
+
+  xhr.onload = () => {
+    if (xhr.status !== 200) {
+      reject(new Error('Error occured'));
+    } else {
+      const result = xhr.response;
+      resolve(result);
+    }
+  };
+
+  xhr.onerror = () => {
+    reject(new Error('Network error'));
+  };
+});
+
+const getJSON = getHttp('https://api.openweathermap.org/data/2.5/weather?q=Mogilev,by&appid=4bed3ee902539e73a03dc1243f44bff9');
+getJSON
+  .then((result) => console.log(JSON.parse(result)))
+  .catch((err) => console.log(err))
+  .finally(() => console.log('Promise is fullfiled'));
